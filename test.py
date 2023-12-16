6d8d8a0bddf97a1f77e4a37403bea3cb04f95667
hiser2009 <hiser2002@yahoo.com>
2023-12-16 02:55:23 -0800

    new feature try.

--- test.py
+++ test.py
@@ -1,116 +1,65 @@
-import requests
-import json
 import os
-import time
+import meraki
 
-# Replace these values with your actual Meraki API key and organization ID
-meraki_api_key = os.getenv("MERAKI_API_KEY")
-org_id = os.getenv("ORG_ID")
-dev_network_name = "Orlando_FL_Branch"  # Manually set the value
+API_KEY = os.getenv('MERAKI_API_KEY')  # Use your environment variable
+ORG_ID = os.getenv('ORG_ID')  # Use your environment variable
 
-# Create Dev network
-url_create_network = f"https://api.meraki.com/api/v1/organizations/{org_id}/networks"
-headers = {
-    "Content-Type": "application/json",
-    "X-Cisco-Meraki-API-Key": meraki_api_key
+dashboard = meraki.DashboardAPI(API_KEY)
+
+# Create network
+network = dashboard.organizations.createOrganizationNetwork(
+    ORG_ID,
+    name='Orlando_FL_Branch',
+    type='combined',
+    timeZone='America/New_York',
+    productTypes=['appliance', 'switch', 'wireless']
+)
+
+# Extract the network ID
+network_id = network['id']
+print(f"Network ID: {network_id}")
+
+# Create VLAN
+vlan_data = {
+    "name": "Voice",
+    "subnet": "192.168.100.0/24",
+    "applianceIp": "192.168.100.1",
+    "groupPolicyId": "101",
+    "templateVlanType": "same",
+    "cidr": "192.168.100.0/24",
+    "mask": 24,
+    "fixedIpAssignments": {
+        "22:33:44:55:66:77": {
+            "ip": "1.2.3.4",
+            "name": "Some client name"
+        }
+    },
+    "reservedIpRanges": [
+        {
+            "start": "192.168.1.1",
+            "end": "192.168.1.100",
+            "comment": "A reserved IP range"
+        }
+    ],
+    "dnsNameservers": "google_dns",
+    "dhcpHandling": "Run a DHCP server",
+    "dhcpLeaseTime": "4 hours",
+    "dhcpBootOptionsEnabled": False,
+    "dhcpBootNextServer": "1.2.3.4",
+    "dhcpBootFilename": "sample.file",
+    "dhcpOptions": [
+        {
+            "code": "5",
+            "type": "text",
+            "value": "five"
+        }
+    ],
+    "adaptivePolicyGroupId": "1234",
+    "dhcpRelayServerIps": ["192.168.1.1/24"],
+    "vpnNatSubnet": "192.168.100.0/24"
 }
-data_create_network = {
-    "name": dev_network_name,
-    "type": "combined",
-    "timeZone": "America/New_York",
-    "productTypes": ["appliance", "switch", "wireless"]
-}
-response_create_network = requests.post(url_create_network, headers=headers, json=data_create_network)
-response_data_create_network = response_create_network.json()
-print("Create Network Response:")
-print(f"Status Code: {response_create_network.status_code}")
-print("Response JSON:")
-print(response_data_create_network)
 
-# Extract the network ID from the response
-dev_network_id = response_data_create_network.get('id')
+response_vlan = dashboard.appliance.updateNetworkApplianceVlan(network_id, vlan_id='', **vlan_data)
 
-if dev_network_id:
-    print(f"Dev Network ID: {dev_network_id}")
-
-    # Add a delay to allow time for the network to be ready
-    time.sleep(5)  # Adjust the delay time as needed
-
-    # Use requests library to create VLANs and DHCP scopes
-    url_create_vlans = f"https://api.meraki.com/api/v1/networks/{dev_network_id}/vlans"
-    payload_vlans = {
-        "name": "My VLAN",
-        "subnet": "192.168.1.0/24",
-        "applianceIp": "192.168.1.2",
-        "groupPolicyId": "101",
-        "templateVlanType": "same",
-        "cidr": "192.168.1.0/24",
-        "mask": 28,
-        "fixedIpAssignments": {
-            "22:33:44:55:66:77": {
-                "ip": "1.2.3.4",
-                "name": "Some client name"
-            }
-        },
-        "reservedIpRanges": [
-            {
-                "start": "192.168.1.0",
-                "end": "192.168.1.1",
-                "comment": "A reserved IP range"
-            }
-        ],
-        "dnsNameservers": "google_dns",
-        "dhcpHandling": "Run a DHCP server",
-        "dhcpLeaseTime": "1 day",
-        "dhcpBootOptionsEnabled": False,
-        "dhcpBootNextServer": "1.2.3.4",
-        "dhcpBootFilename": "sample.file",
-        "dhcpOptions": [
-            {
-                "code": "5",
-                "type": "text",
-                "value": "five"
-            }
-        ],
-        "ipv6": {
-            "enabled": True,
-            "prefixAssignments": [
-                {
-                    "autonomous": False,
-                    "staticPrefix": "2001:db8:3c4d:15::/64",
-                    "staticApplianceIp6": "2001:db8:3c4d:15::1",
-                    "origin": {
-                        "type": "internet",
-                        "interfaces": ["wan0"]
-                    }
-                }
-            ]
-        },
-        "mandatoryDhcp": {"enabled": True},
-        "adaptivePolicyGroupId": "1234",
-        "dhcpRelayServerIps": [
-            "192.168.1.0/24",
-            "192.168.128.0/24"
-        ],
-        "vpnNatSubnet": "192.168.1.0/24"
-    }
-
-    headers_vlans = {
-        "Authorization": f"Bearer {meraki_api_key}",
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    }
-
-    response_vlans = requests.post(url_create_vlans, headers=headers_vlans, json=payload_vlans)
-    print("Create VLANs Response:")
-    print(f"Status Code: {response_vlans.status_code}")
-    print("Response JSON:")
-    print(response_vlans.text.encode('utf8'))
-
-    # Continue with other tasks...
-    # ...
-
-    print("Dev deployment successful!")
-
-else:
-    print("Failed to extract Dev Network ID from the response.")
+print("Create VLAN Response:")
+print(response_vlan)
