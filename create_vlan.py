4ea2008e3e6bc6606426e4afbfca49b63c10b0e9
hiser2009 <hiser2002@yahoo.com>
2023-12-19 21:48:33 -0800

    Create Networks with DHCP scopes.

--- create_vlan.py
+++ create_vlan.py
@@ -27,15 +27,36 @@ def create_vlan(network_id, vlan_id, vlan_name, subnet, appliance_ip):
         response = dashboard.appliance.createNetworkApplianceVlan(network_id, **vlan_data)
         print(f"VLAN '{vlan_name}' created successfully:")
         print(response)
+
+        # Create DHCP scope for the VLAN
+        create_dhcp_scope(network_id, vlan_id, subnet)
     except meraki.APIError as e:
         print(f"Error creating VLAN '{vlan_name}': {e}")
 
+def create_dhcp_scope(network_id, vlan_id, subnet):
+    try:
+        dhcp_data = {
+            "vlanId": vlan_id,
+            "subnet": subnet,
+            "applianceIp": f"{subnet.split('.')[0]}.{subnet.split('.')[1]}.{vlan_id}.1",
+            "minIpAddress": f"{subnet.split('.')[0]}.{subnet.split('.')[1]}.{vlan_id}.100",
+            "maxIpAddress": f"{subnet.split('.')[0]}.{subnet.split('.')[1]}.{vlan_id}.254",
+            "defaultGateway": f"{subnet.split('.')[0]}.{subnet.split('.')[1]}.{vlan_id}.1",
+            "dnsNameservers": "upstream_dns"
+        }
+
+        response = dashboard.appliance.createNetworkApplianceVlanL3FirewallRules(network_id, vlan_id, rules=[dhcp_data])
+        print(f"DHCP scope for VLAN '{vlan_id}' created successfully:")
+        print(response)
+    except meraki.APIError as e:
+        print(f"Error creating DHCP scope for VLAN '{vlan_id}': {e}")
+
 if __name__ == "__main__":
     vlan_settings = [
-        {"id": 100, "name": "VLAN1", "subnet": "192.168.10.0/24", "appliance_ip": "192.168.10.1"},
-        {"id": 101, "name": "VLAN2", "subnet": "192.168.20.0/24", "appliance_ip": "192.168.20.1"},
-        {"id": 102, "name": "VLAN3", "subnet": "192.168.30.0/24", "appliance_ip": "192.168.30.1"},
-        {"id": 103, "name": "VLAN4", "subnet": "192.168.40.0/24", "appliance_ip": "192.168.40.1"}
+        {"id": 100, "name": "VLAN10", "subnet": "192.168.10.0/24", "appliance_ip": "192.168.10.1"},
+        {"id": 101, "name": "VLAN20", "subnet": "192.168.20.0/24", "appliance_ip": "192.168.20.1"},
+        {"id": 102, "name": "VLAN30", "subnet": "192.168.30.0/24", "appliance_ip": "192.168.30.1"},
+        {"id": 103, "name": "VLAN40", "subnet": "192.168.40.0/24", "appliance_ip": "192.168.40.1"}
     ]
 
     enable_vlans(NETWORK_ID)
