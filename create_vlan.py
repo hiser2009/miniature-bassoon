617f680feaa1f5dd5b8d5f8d86d5e5df75c0280a
hiser2009 <hiser2002@yahoo.com>
2023-12-19 21:52:36 -0800

    Create Networks with DHCP scopes.

--- create_vlan.py
+++ create_vlan.py
@@ -45,7 +45,7 @@ def create_dhcp_scope(network_id, vlan_id, subnet):
             "dnsNameservers": "upstream_dns"
         }
 
-        response = dashboard.appliance.createNetworkApplianceVlanL3FirewallRules(network_id, vlan_id, rules=[dhcp_data])
+        response = dashboard.appliance.updateNetworkApplianceFirewallL3FirewallRules(network_id, vlan_id, rules=[dhcp_data])
         print(f"DHCP scope for VLAN '{vlan_id}' created successfully:")
         print(response)
     except meraki.APIError as e:
@@ -53,10 +53,10 @@ def create_dhcp_scope(network_id, vlan_id, subnet):
 
 if __name__ == "__main__":
     vlan_settings = [
-        {"id": 100, "name": "VLAN10", "subnet": "192.168.10.0/24", "appliance_ip": "192.168.10.1"},
-        {"id": 101, "name": "VLAN20", "subnet": "192.168.20.0/24", "appliance_ip": "192.168.20.1"},
-        {"id": 102, "name": "VLAN30", "subnet": "192.168.30.0/24", "appliance_ip": "192.168.30.1"},
-        {"id": 103, "name": "VLAN40", "subnet": "192.168.40.0/24", "appliance_ip": "192.168.40.1"}
+        {"id": 100, "name": "VLAN1", "subnet": "192.168.10.0/24", "appliance_ip": "192.168.10.1"},
+        {"id": 101, "name": "VLAN2", "subnet": "192.168.20.0/24", "appliance_ip": "192.168.20.1"},
+        {"id": 102, "name": "VLAN3", "subnet": "192.168.30.0/24", "appliance_ip": "192.168.30.1"},
+        {"id": 103, "name": "VLAN4", "subnet": "192.168.40.0/24", "appliance_ip": "192.168.40.1"}
     ]
 
     enable_vlans(NETWORK_ID)
