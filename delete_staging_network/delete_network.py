45e3808433f4aa817c74f4b493305b14b03932e6
hiser2009 <hiser2002@yahoo.com>
2023-12-22 19:35:34 -0800

    Add Las Vegas Network.

--- delete_staging_network/delete_network.py
+++ delete_staging_network/delete_network.py
@@ -3,10 +3,20 @@ import meraki
 
 API_KEY = os.getenv('MERAKI_API_KEY')  # Replace with your actual Meraki API key
 # NETWORK_ID = 'N_705376291636949794'  # Dummy Network
-NETWORK_ID = os.getenv('CREATED_NETWORK_ID')  # Retrieve the value of CREATED_NETWORK_ID
+# NETWORK_ID = os.getenv('CREATED_NETWORK_ID')  # Retrieve the value of CREATED_NETWORK_ID
+NETWORK_ID_FILE = 'created_network_id.txt'  # File containing the created network ID
+
 
 dashboard = meraki.DashboardAPI(API_KEY)
 
+# Function to read the created network ID from the file
+def read_created_network_id():
+    try:
+        with open(NETWORK_ID_FILE, 'r') as file:
+            return file.read().strip()
+    except FileNotFoundError:
+        return None
+    
 def delete_network(network_id):
     try:
         dashboard.networks.deleteNetwork(
@@ -17,4 +27,9 @@ def delete_network(network_id):
         print(f"Error deleting network: {e}")
 
 if __name__ == "__main__":
-    delete_network(NETWORK_ID)
+    CREATED_NETWORK_ID = read_created_network_id()
+    if CREATED_NETWORK_ID:
+        delete_network(CREATED_NETWORK_ID)
+    else:
+        print("Environment variable CREATED_NETWORK_ID not set.")
+
